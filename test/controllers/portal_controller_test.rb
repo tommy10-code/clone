aa56f97249ed0203799a29a0ebd56b6a4e38da4e
tommy10-code <mtywp058@gmail.com>
2025-08-07 17:10:29 +0900

    地図表示まで

---
+++ test/controllers/portal_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class PortalControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get portal_index_url
+    assert_response :success
+  end
+end
