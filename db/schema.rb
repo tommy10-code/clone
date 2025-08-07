d444050d205c00091f212ee377854045f7c2dd54
tommy10-code <mtywp058@gmail.com>
2025-08-07 21:36:41 +0900

    検索機能とカテゴリ追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_04_090031) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_07_085711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_04_090031) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -52,6 +58,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_04_090031) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "tasks", force: :cascade do |t|
