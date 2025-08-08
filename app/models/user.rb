0dbd932cfd36e23effbaab4cdaaa8a2c14e27e7e
tommy10-code <mtywp058@gmail.com>
2025-08-08 13:52:35 +0900

    お気に入り非同期設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_shops, through: :favorites, source: :shop
+
+  def bookmark(shop) # お気に入り追加 かつ 重複登録の防止
+    favorite_shops << shop unless bookmark?(shop)
+	end
+
+  def unbookmark(shop)# お気に入り削除
+    favorite_shops.destroy(shop)
+  end
+
+  def bookmark?(shop)#お気に入りがすでに登録されているか確認
+    favorite_shops.include?(shop)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :shops
-  	authenticated :user do
-	    root to: 'shops#index', as: :authenticated_root
-    end
+  resources :users, only: [:index, :show]
+  resources :shops do
+    resource :favorites, only: [:create, :destroy]
+  end
+  authenticated :user do
+    root to: 'shops#index', as: :authenticated_root
+  end
+
+  get '/favorites', to: 'users#favorites'
   get "diagnoses/new"
   post "diagnosis/result", to: "diagnoses#result", as: :diagnosis_result
-  
-  resources :users, only: [:index, :show]
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
