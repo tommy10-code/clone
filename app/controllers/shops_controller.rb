9fa6eb2191020c2b1d50244ee9e2dd94228d1be9
tommy10-code <mtywp058@gmail.com>
2025-08-09 16:52:51 +0900

    詳細ページの変更

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -29,11 +29,12 @@ class ShopsController < ApplicationController
 
   def update
 	  @shop = Shop.find(params[:id])
-      if @shop.update(shop_params)
-        redirect_to shop_path(@shop), notice: "User was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
+
+    if @shop.update(shop_params)
+      redirect_to shop_path(@shop), notice: "User was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
  def destroy
