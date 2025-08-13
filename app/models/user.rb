f08f489fcff263031b0ef3289f953cfe46dfd3cf
tommy10-code <mtywp058@gmail.com>
2025-08-13 13:19:19 +0900

    アクセス制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
           :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-
+  has_many :shops, dependent: :nullify
   has_many :favorites, dependent: :destroy
   has_many :favorite_shops, through: :favorites, source: :shop
 
