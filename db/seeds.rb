2e1d7e05ab2f41f9e6758010ccb7e0c66c6019fa
tommy10-code <mtywp058@gmail.com>
2025-08-13 17:55:59 +0900

    お店登録前

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-Category.create!(name: "イタリアン")
-Category.create!(name: "フレンチ")
-Category.create!(name: "和食・割烹")
-Category.create!(name: "韓国料理")
-Category.create!(name: "中華・四川")
-Category.create!(name: "カフェ・喫茶店")
-Category.create!(name: "バー・ダイニングバー")
-Category.create!(name: "ビストロ")
-Category.create!(name: "パンケーキ・スイーツ系")
-Category.create!(name: "居酒屋・創作料理")
+# Category.create!(name: "イタリアン")
+# Category.create!(name: "フレンチ")
+# Category.create!(name: "和食・割烹")
+# Category.create!(name: "韓国料理")
+# Category.create!(name: "中華・四川")
+# Category.create!(name: "カフェ・喫茶店")
+# Category.create!(name: "バー・ダイニングバー")
+# Category.create!(name: "ビストロ")
+# Category.create!(name: "パンケーキ・スイーツ系")
+# Category.create!(name: "居酒屋・創作料理")
 
-scenes = %w[初デート 2回目以降 記念日 サク飯 静かに話せる 大人っぽい]
+categories = %w[  イタリアン フレンチ 和食・割烹 韓国料理 中華・四川 カフェ・喫茶店 バー・ダイニングバー ビストロ パンケーキ・スイーツ系 居酒屋・創作料理]
+categories.each do |n|
+  Category.find_or_create_by!(name: n)
+end
+
+
+scenes = %w[初デート 個室有 隠れ家 照明やや暗め ]
 scenes.each do |n|
   Scene.find_or_create_by!(name: n) do |s|
     s.slug = n.parameterize # slug使うなら
