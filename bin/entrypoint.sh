1caddeb1ba6835bce764343968ee35b7b5dcff65
tommy10-code <mtywp058@gmail.com>
2025-08-15 11:49:20 +0900

    アドレス修正2

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-# この下にやりたいコマンドを書く
 echo "マイグレーション実行中..."
 bundle exec rails db:migrate
 
-echo "必要ならseed実行..."
-bundle exec rails db:seed
+if [ "${SEED_ON_BOOT}" = "true" ]; then
+  echo "== db:seed 実行 =="
+  bundle exec rails db:seed
+  echo "== db:seed 完了 =="
+else
+  echo "seedはスキップ（SEED_ON_BOOT=true のときだけ実行）"
+fi
 
 echo "アプリ起動"
 exec bundle exec puma -C config/puma.rb
