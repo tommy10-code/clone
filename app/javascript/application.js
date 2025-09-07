526036755d744b2d7d74e1e3fe0ccffcd3cff3a5
tommy10-code <mtywp058@gmail.com>
2025-09-07 13:41:37 +0900

    20partialがpushしていなかったので実行

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import { Autocomplete } from 'stimulus-autocomplete'  // 追加
+
+const application = Application.start() // 追加
+application.register('autocomplete', Autocomplete) // 追加
