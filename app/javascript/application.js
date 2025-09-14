f7bce6ccb39a2c509f6fcbd98cd8e1d096f154a5
tommy10-code <mtywp058@gmail.com>
2025-09-14 13:10:46 +0900

    jsやり直し前

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import "./map"
 import { Application } from "@hotwired/stimulus"
 import { Autocomplete } from 'stimulus-autocomplete'  // 追加
 
