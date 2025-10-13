4059ba30cf31a0dbaaef50d705de4dc7c1252b4c
tommy10-code <mtywp058@gmail.com>
2025-10-13 12:16:44 +0900

    stimulusにCSSを導入

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -2,9 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["input", "output"]
+  static values = { min: Number }
 
   update() {
     const length = this.inputTarget.value.length
-    this.outputTarget.textContent = `現在の文字数 ${length}`
+    
+    if ( length < this.minValue) {
+      this.outputTarget.textContent = `あと ${this.minValue - length} 文字必要です`
+      //this.outputTarget.classList.add("text-red-600")
+      //this.outputTarget.classList.remove("text-green-600")
+    } else {
+    this.outputTarget.textContent = `OK!`
+      //this.outputTarget.classList.add("text-green-600")
+      //this.outputTarget.classList.remove("text-red-600")
+    }
+    this.outputTarget.classList.toggle("text-red-600", length < this.minValue)
+    this.outputTarget.classList.toggle("text-green-600", length >= this.minValue)
+
   }
 }
