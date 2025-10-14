07327dcbefeb308b80f7bc8a567cee03c2f85223
tommy10-code <mtywp058@gmail.com>
2025-10-14 13:44:50 +0900

    AcitveMailerの修正3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,15 +46,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.sendgrid.net",
-    port:                 465,
-    ssl: true,
-    tls: true,
+    port:                 587,
     domain:               "dokodate.jp",
     user_name:            "apikey",
     password:             ENV["SENDGRID_PASSWORD"],
     authentication:       :plain,
     enable_starttls_auto: true
-    open_timeout: 10,
-    read_timeout: 10
   }
 end
