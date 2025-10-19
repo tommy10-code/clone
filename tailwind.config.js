bdb8ae88a7f3c6193b58f168e23e0bce4c5ac109
tommy10-code <mtywp058@gmail.com>
2025-10-19 15:54:25 +0900

    スマホの色修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ],
-  plugins: [
-    require("daisyui")],
+  ]
+  // plugins: [
+    // require("daisyui")],
 }
