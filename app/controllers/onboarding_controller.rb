061810da3d167cef634e47c14037d7deea244304
tommy10-code <mtywp058@gmail.com>
2025-10-25 17:58:12 +0900

    コントローラー設定

---
+++ app/controllers/onboarding_controller.rb
@@ -0,0 +1,12 @@
+class OnboardingController < ApplicationController
+  before_action :authenticate_user!
+
+  def update
+    #受け取ったパラメータがtrueならっていう処理 trueに変更、違うなら何もしない
+    if params[:hide_onboarding_banner] == true || params[:hide_onboarding_banner] == "true"
+      current_user.update(hide_onboarding_banner: true)
+    end
+    head :no_content
+    end
+  end
+end
