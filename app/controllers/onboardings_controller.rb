d45ddb599a2c9c94f5917d9d065a6be3b62d9718
tommy10-code <mtywp058@gmail.com>
2025-10-25 19:39:53 +0900

    rubocop修正

--- app/controllers/onboardings_controller.rb
+++ app/controllers/onboardings_controller.rb
@@ -2,7 +2,6 @@ class OnboardingsController < ApplicationController
   before_action :authenticate_user!
 
   def update
-    #受け取ったパラメータがtrueならっていう処理 trueに変更、違うなら何もしない
     if params[:hide_onboarding_banner] == true || params[:hide_onboarding_banner] == "true"
       current_user.update(hide_onboarding_banner: true)
     end
